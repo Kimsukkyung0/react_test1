0925eb959f6e74f38707b52149a9bed00d9252bd
Min kyung suk <smk93021@gmail.com>
2024-01-07 20:08:46 +0900

    sample+

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return <div className='App'>'Hello React!';</div>;
+    //리턴시 하나의 태그를 포함해야함 플레인텍스트도 안됨!
+    //ex1: return <header></header><div></div> ---error
+    //ex2: return Hello React! ---error
+
+
+
+    
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  }
 }
 
 export default App;
